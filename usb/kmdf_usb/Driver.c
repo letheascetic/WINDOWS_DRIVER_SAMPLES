3440f7a2093da212751b9300c5d9e720c8925692
letheascetic <yizhifight@163.com>
2019-01-08 21:21:48 +0800

    update

--- usb/kmdf_usb/Driver.c
+++ usb/kmdf_usb/Driver.c
@@ -17,9 +17,11 @@ Environment:
 #include "private.h"
 #include "driver.tmh"
 
+PFN_IO_GET_ACTIVITY_ID_IRP g_pIoGetActivityIdIrp;
+PFN_IO_SET_DEVICE_INTERFACE_PROPERTY_DATA g_pIoSetDeviceInterfacePropertyData;
+
 #ifdef ALLOC_PRAGMA
 #pragma alloc_text (INIT, DriverEntry)
-#pragma alloc_text (PAGE, KmdfUsbEvtDeviceAdd)
 #pragma alloc_text (PAGE, KmdfUsbEvtDriverContextCleanup)
 #endif
 
@@ -55,16 +57,33 @@ Return Value:
 
 --*/
 {
-    WDF_DRIVER_CONFIG config;
-    NTSTATUS status;
-    WDF_OBJECT_ATTRIBUTES attributes;
+    WDF_DRIVER_CONFIG		config;
+    NTSTATUS				status;
+    WDF_OBJECT_ATTRIBUTES	attributes;
+	UNICODE_STRING          funcName;
 
     //
     // Initialize WPP Tracing
     //
     WPP_INIT_TRACING( DriverObject, RegistryPath );
 
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
+	TraceEvents(TRACE_LEVEL_INFORMATION, DBG_INIT, "KMDF USB Driver Sample - Driver Framework Edition.\n");
+
+	//
+	// IRP activity ID functions are available on some versions, save them into
+	// globals (or NULL if not available)
+	//
+	RtlInitUnicodeString(&funcName, L"IoGetActivityIdIrp");
+	g_pIoGetActivityIdIrp = (PFN_IO_GET_ACTIVITY_ID_IRP)(ULONG_PTR)
+		MmGetSystemRoutineAddress(&funcName);
+
+	//
+	// The Device interface property set is available on some version, save it
+	// into globals (or NULL if not available)
+	//
+	RtlInitUnicodeString(&funcName, L"IoSetDeviceInterfacePropertyData");
+	g_pIoSetDeviceInterfacePropertyData = (PFN_IO_SET_DEVICE_INTERFACE_PROPERTY_DATA)(ULONG_PTR)
+		MmGetSystemRoutineAddress(&funcName);
 
     //
     // Register a cleanup callback so that we can call WPP_CLEANUP when
@@ -85,54 +104,24 @@ Return Value:
                              );
 
     if (!NT_SUCCESS(status)) {
-        TraceEvents(TRACE_LEVEL_ERROR, TRACE_DRIVER, "WdfDriverCreate failed %!STATUS!", status);
+		TraceEvents(TRACE_LEVEL_ERROR, DBG_INIT, "WdfDriverCreate failed with status 0x%x\n", status);
+		//
+		// Cleanup tracing here because DriverContextCleanup will not be called
+		// as we have failed to create WDFDRIVER object itself.
+		// Please note that if your return failure from DriverEntry after the
+		// WDFDRIVER object is created successfully, you don't have to
+		// call WPP cleanup because in those cases DriverContextCleanup
+		// will be executed when the framework deletes the DriverObject.
+		//
         WPP_CLEANUP(DriverObject);
         return status;
     }
 
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Exit");
+    TraceEvents(TRACE_LEVEL_INFORMATION, DBG_INIT, "%!FUNC! Exit");
 
     return status;
 }
 
-NTSTATUS
-KmdfUsbEvtDeviceAdd(
-    _In_    WDFDRIVER       Driver,
-    _Inout_ PWDFDEVICE_INIT DeviceInit
-    )
-/*++
-Routine Description:
-
-    EvtDeviceAdd is called by the framework in response to AddDevice
-    call from the PnP manager. We create and initialize a device object to
-    represent a new instance of the device.
-
-Arguments:
-
-    Driver - Handle to a framework driver object created in DriverEntry
-
-    DeviceInit - Pointer to a framework-allocated WDFDEVICE_INIT structure.
-
-Return Value:
-
-    NTSTATUS
-
---*/
-{
-    NTSTATUS status;
-
-    UNREFERENCED_PARAMETER(Driver);
-
-    PAGED_CODE();
-
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
-
-    status = KmdfUsbCreateDevice(DeviceInit);
-
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Exit");
-
-    return status;
-}
 
 VOID
 KmdfUsbEvtDriverContextCleanup(
@@ -153,15 +142,20 @@ Return Value:
 
 --*/
 {
-    UNREFERENCED_PARAMETER(DriverObject);
+	//
+	// EvtCleanupCallback for WDFDRIVER is always called at PASSIVE_LEVEL
+	//
+	_IRQL_limited_to_(PASSIVE_LEVEL);
 
     PAGED_CODE ();
 
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
+	TraceEvents(TRACE_LEVEL_INFORMATION, DBG_INIT, "--> KmdfUsbEvtDriverContextCleanup\n");
 
     //
     // Stop WPP Tracing
     //
     WPP_CLEANUP( WdfDriverWdmGetDriverObject( (WDFDRIVER) DriverObject) );
+
+	UNREFERENCED_PARAMETER(DriverObject);
 
 }
